18e11423856e550271aedd8eebef4570849d7bd9
hung1fps <Hung.LNQ225975@sis.hust.edu.vn>
2024-04-01 12:23:09 +0700

    commit

--- logsitic_test.py
+++ logsitic_test.py
@@ -1,5 +1,15 @@
-import pickle 
-import numpy as np
-from logistic import features_list
-with open('data_test.pkl', 'rb') as f:
-    features = pickle.load(f)
+import tensorflow as tf
+import pandas as pd
+import matplotlib
+from matplotlib import pyplot as plt
+import seaborn as sns
+import sklearn.metrics as sk_metrics
+import tempfile
+import os
+
+# Preset matplotlib figure sizes.
+matplotlib.rcParams['figure.figsize'] = [9, 6]
+
+print(tf.__version__)
+# To make the results reproducible, set the random seed value.
+tf.random.set_seed(22)
